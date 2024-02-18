b829d22a98d460f41d0de087668cedc229649efc
choi <choiseungho11@hanmail.net>
2024-02-18 20:24:16 +0900

    test

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ var mysql = require('mysql');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
