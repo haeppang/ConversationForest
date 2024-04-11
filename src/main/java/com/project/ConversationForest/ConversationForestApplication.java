7486cca3e580247e482c715e718dde52b9bea014
OllyDI <choiseungho11@hanmail.net>
2024-04-11 15:56:19 +0900

    24.04.11

---
+++ src/main/java/com/project/ConversationForest/ConversationForestApplication.java
@@ -0,0 +1,13 @@
+package com.project.ConversationForest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConversationForestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConversationForestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/ConversationForest/ConversationForestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ConversationForest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversationForestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
